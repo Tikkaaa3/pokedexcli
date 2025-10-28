e618d3d1734059f11c291cddea7d559a79fd37ab
Tikkaaa3 <tikkaaa3@gmail.com>
2025-10-28 16:25:06 +0100

    feat: add caching and map commands

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"github.com/Tikkaaa3/pokedexcli/internal/pokeapi"
+	"time"
+)
+
 func main() {
-	startRepl()
+	pokeClient := pokeapi.NewClient(5*time.Second, time.Minute*5)
+	cfg := &config{
+		pokeapiClient: pokeClient,
+	}
+
+	startRepl(cfg)
 }
--- repl.go
+++ repl.go
@@ -5,22 +5,32 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/Tikkaaa3/pokedexcli/internal/pokeapi"
 )
 
-func startRepl() {
-	scanner := bufio.NewScanner(os.Stdin)
+type config struct {
+	pokeapiClient    pokeapi.Client
+	nextLocationsURL *string
+	prevLocationsURL *string
+}
+
+func startRepl(cfg *config) {
+	reader := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		text := scanner.Text()
-		words := cleanInput(text)
+		reader.Scan()
+
+		words := cleanInput(reader.Text())
 		if len(words) == 0 {
 			continue
 		}
+
 		commandName := words[0]
+
 		command, exists := getCommands()[commandName]
 		if exists {
-			err := command.callback()
+			err := command.callback(cfg)
 			if err != nil {
 				fmt.Println(err)
 			}
@@ -29,20 +39,19 @@ func startRepl() {
 			fmt.Println("Unknown command")
 			continue
 		}
-
 	}
 }
 
 func cleanInput(text string) []string {
-	lower := strings.ToLower(text)
-	clean := strings.Fields(lower)
-	return clean
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
 }
 
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
 }
 
 func getCommands() map[string]cliCommand {
@@ -51,6 +60,16 @@ func getCommands() map[string]cliCommand {
 			name:        "help",
 			description: "Displays a help message",
 			callback:    commandHelp,
+		},
+		"map": {
+			name:        "map",
+			description: "Get the next page of locations",
+			callback:    commandMapf,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Get the previous page of locations",
+			callback:    commandMapb,
 		},
 		"exit": {
 			name:        "exit",
